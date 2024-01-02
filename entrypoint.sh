283f77954174f0f6061eb848f64157194914b3cd
John Wass <jwass3@gmail.com>
2024-01-02 08:03:18 -0500

    cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-python /action/check.py --categories=${INPUT_CATEGORIES}         \
+cd /action
+
+python check.py --categories=${INPUT_CATEGORIES}         \
                 --ignores=${INPUT_IGNORES}               \
                 --contrib_guide_url=${INPUT_CONTRIB_URL} \
                 ${INPUT_ARTICLE_DIR}
