477ec808f3c0fafe4795a4d769c7c95f6468eb39
John Wass <jwass3@gmail.com>
2024-01-03 16:20:16 -0500

    Fix article dir calculation (#3)
    The article dir was being distorted in the entry point script
    
    This fixes the issue by performing the calculation prior to changing to
    the action dir in the container.
    
    This also adds a debug flag to print out additional information when
    working through an issue.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+ARTICLE_DIR="${PWD}/${INPUT_ARTICLE_DIR}"
+
+if [[ "${INPUT_DEBUG,,}" == "true" ]]; then
+  echo "=== Debug Mode Enabled ==="
+  echo "Article dir: ${ARTICLE_DIR}"
+  ls -al ${ARTICLE_DIR}
+fi
+
 cd /action
 
 python check.py --categories=${INPUT_CATEGORIES}         \
                 --ignores=${INPUT_IGNORES}               \
                 --contrib_guide_url=${INPUT_CONTRIB_URL} \
-                ${INPUT_ARTICLE_DIR}
+                ${ARTICLE_DIR}
--- test.py
+++ test.py
@@ -6,16 +6,18 @@ import check as checker
 # returns:
 #  1: OK
 #  0: Not OK
-def check(event, ignores=None, categories=None, contrib_url=None):
+def passing(event, dir, ignores=None, categories=None, contrib_url=None):
     os.environ["EVENT_PATH"] = f"test/{event}.json"
-    return not checker.main("test", ignores, categories, contrib_url)
+    return not checker.main(f"test/{dir}", ignores, categories, contrib_url)
 
 
 def test_simple():
-    assert not check("e0")
+    assert passing("e0", "news1")
+    assert not passing("e3", "news1")
+    assert passing("e3", "news3")
 
 
 def test_ignore():
-    assert not check("e2")
-    assert check("e2", ignores="ignore")
-    assert check("e2", ignores="ignore,foo")
+    assert not passing("e2", "news2")
+    assert passing("e2", "news2", ignores="ignore")
+    assert passing("e2", "news2", ignores="ignore,foo")
