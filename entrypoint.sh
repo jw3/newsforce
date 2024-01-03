24f914772d6f43364ab4f4df1190a4bd27ae498a
John Wass <jwass3@gmail.com>
2024-01-03 13:18:51 -0500

    debug flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ "${INPUT_DEBUG,,}" == "true" ]]; then
+  echo "=== Debug Mode Enabled ==="
+  ls -al ${INPUT_ARTICLE_DIR}
+fi
+
 cd /action
 
 python check.py --categories=${INPUT_CATEGORIES}         \
