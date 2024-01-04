fd7a248dbce46a7a742644649595ed5b153641ea
John Wass <jwass3@gmail.com>
2024-01-04 07:03:54 -0500

    caseless ignore checks

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ import check as checker
 # returns:
 #  1: OK
 #  0: Not OK
-def passing(event, dir, ignores=None, categories=None, contrib_url=None):
+def passing(event, article_dir, ignores=None, categories=None, contrib_url=None):
     os.environ["EVENT_PATH"] = f"test/{event}.json"
-    return not checker.main(f"test/{dir}", ignores, categories, contrib_url)
+    return not checker.main(f"test/{article_dir}", ignores, categories, contrib_url)
 
 
 def test_simple():
@@ -21,3 +21,10 @@ def test_ignore():
     assert not passing("e2", "news2")
     assert passing("e2", "news2", ignores="ignore")
     assert passing("e2", "news2", ignores="ignore,foo")
+
+
+def test_case():
+    assert passing("e0", "news2", ignores="B00M")
+    assert passing("e0", "news2", ignores="b00m")
+    assert passing("e2", "news2", ignores="ignore")
+    assert passing("e2", "news2", ignores="IGNORE")
